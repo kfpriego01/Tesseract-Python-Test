359e29fee0bb5a445e9aba438ce19db3ee4e5719
kfpriego01 <diocampojoanjoy24@gmail.com>
2025-08-20 11:08:16 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pytesseract
+
+# Tell pytesseract where tesseract.exe is installed
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Load an image
+image = Image.open("IvV2y.png")
+
+# Extract text
+text = pytesseract.image_to_string(image)
+
+print("Extracted Text:")
+print(text)
